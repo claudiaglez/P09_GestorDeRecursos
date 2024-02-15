bb77f70c9f34d65dcad0257b6cb06790cb9164e6
Claudia González <claudia.glezg@gmail.com>
2024-02-15 10:22:58 +0100

    create views and add navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+@extends('templates.headerTemplate')
+@section('navbar')
+<h1>Hola soy Index</h1>
+@endsection
+
+<script src="../path/to/flowbite/dist/flowbite.min.js"></script>   
+</body>
+</html>
